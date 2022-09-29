79fdd8141fba09aa4eb31331739748a279cd5739
KristinaHandler <kristina.handler2806@gmail.com>
2022-09-29 13:43:05 +0200

    Update 4.Spleen_preliminary_analysis.R

--- Sphere-seq_analysis/4.Spleen_preliminary_analysis.R
+++ Sphere-seq_analysis/4.Spleen_preliminary_analysis.R
@@ -63,43 +63,42 @@ ElbowPlot(merged)
 
 #cluster 
 merged <- FindNeighbors(object = merged, dims = 1:10, reduction = "pca")
-merged_cl <- FindClusters(merged, resolution = 1, random.seed = 5, algorithm = 1, graph.name = "SCT_snn")
+merged_cl <- FindClusters(merged, resolution = 0.5, random.seed = 5, algorithm = 4, graph.name = "SCT_snn")
 merged_cl <- RunUMAP(merged_cl, dims = 1:10, seed.use = 5, reduction = "pca")
 DimPlot(merged_cl, reduction = "umap", label = TRUE, pt.size = .5, group.by = "seurat_clusters") + ggsave("./figures/4/umap_clustering.pdf")
 
 ########## Annotation based on marker genes from Medaglia et al 2017 Science ##########
 DotPlot(merged_cl, features = c(
-  "Cd79b", "Ly6d", # B cluster 0,3,
-  "Cr2", #B Cd21 high  cluster 1
-  "Sdc1", #Plasma cells PC cluster 4
-  "Trbc2","Tcf7", "Igfbp4", #CD4 T cluster 2
-  "Cd8a", #CD8 T  cluster 2
-  "Gzma","Ccl5", #NK cluster 7
-  "S100a4", #Mono Ly6c low cluster 11
-  "Ccr2","Lyz2", #Mono Ly6c high cluster 11
-  "Cd5l","Vcam1", #Mac cluster 12
-  "Bst2", #pDC cluster 15 
-  "Fscn1", #DC cluster 5
-  "S100a8", #Neut cluster 13,
-  "Hba-a2" #RBC cluster 12,18
+  "Cd79b", "Ly6d", # B cluster 1,4,5,8
+  "Cr2", #B Cd21 high  cluster 3
+  "Sdc1", #Plasma cells PC 
+  "Trbc2","Tcf7", "Igfbp4", #CD4 T cluster 7
+  "Cd8a", #CD8 T  cluster 2,10,17
+  "Gzma","Ccl5", #NK cluster 9
+  "S100a4", #Mono Ly6c low cluster 6
+  "Ccr2","Lyz2", #Mono Ly6c high cluster 12
+  "Cd5l","Vcam1", #Mac cluster 13
+  "Bst2", #pDC cluster 16
+  "Fscn1", #DC cluster none
+  "S100a8", #Neut cluster 15
+  "Hba-a2" #RBC cluster 11
 ) )+   theme(axis.text.x = element_text(angle = 90)) + ggsave("./figures/4/marker_clustering_dotplot.pdf")
 
-current.cluster.ids <- c(0:19)
-new.cluster.ids <- c("B","B","T_CD8","B","B_CD21_high","B","Mono_Ly6c_low","T_CD4","T_CD4",
-                     "NK","T_CD8","B","RBC","Mono_Ly6c_high","Macrophages","B","Neutrophils","pDC",
-                     "RBC","PC")
+current.cluster.ids <- c(1:17)
+new.cluster.ids <- c("B","CD8_T","B_CD21_high","B","B","Mono_Ly6c_low","CD4_T","B","NK",
+                     "CD8_T","RBC", "Mono_Ly6c_high","Macrophages","?","Neutrophils","pDC","CD8_T")
 merged_cl$annotation <- plyr::mapvalues(x = merged_cl$seurat_clusters, from = current.cluster.ids, to = new.cluster.ids)
+
+#remove RBCs 
+Idents(merged_cl) <- "annotation"
+merged_cl <- subset(merged_cl, idents = c("B","B_CD21_high", "CD8_T","CD4_T","Mono_Ly6c_low","NK","Mono_Ly6c_high","Macrophages","Neutrophils","pDC"))
 
 #T cell areas: red colours:  CD4 T , CD8 T , NK 
 #B cell areas: Blue colours:  B, B CD21 high, PC 
 #Marginal zones MZ: purple colours: Mono Ly6c low, mono ly6c high, Mac, pDC, Neut
 
-current.cluster.ids <- c("B","T_CD8","B_CD21_high","Mono_Ly6c_low","T_CD4",
-                         "NK","RBC","Mono_Ly6c_high","Macrophages","Neutrophils","pDC",
-                         "PC")
-new.cluster.ids <- c("B","T","B","MZ","T",
-                     "T","RBC","MZ","MZ","MZ","MZ",
-                     "B")
+current.cluster.ids <- c("B","B_CD21_high", "CD8_T","CD4_T","Mono_Ly6c_low","NK","Mono_Ly6c_high","Macrophages","Neutrophils","pDC")
+new.cluster.ids <- c("B","B", "T","T","MZ","T","MZ","MZ","MZ","MZ")
 merged_cl$zone <- plyr::mapvalues(x = merged_cl$annotation, from = current.cluster.ids, to = new.cluster.ids)
 
 #remove RBCs
@@ -108,7 +107,16 @@ merged_cl <- subset(merged_cl, idents = c("B","T","MZ"))
 
 #add zone into meta data based on cell type presence 
 DimPlot(merged_cl, group.by = "zone",label = TRUE) + ggsave("./figures/4/zone_umap.pdf")
-DimPlot(merged_cl, group.by = "annotation",label = TRUE) + ggsave("./figures/4/annotated_umap.pdf")
+p <- DimPlot(merged_cl, group.by = "annotation",label = TRUE, cols = c("#1C3EE5","#EA32EA","#1CA7E5", "#D2E51C",  "#B50926",
+                                                                       "#F9AFC0","#E5A21C",
+                                                                       "#EFCD8B","#F2EB08","#EFEF8B" )) 
+p + ggsave("./figures/4/annotated_umap.pdf")
+p + ggsave("./figures/4/annotated_umap.svg")
+
+###Colours
+#T cell areas: red colors:  CD4 T #B50926, CD8 T#EA32EA , NK #F9AFC0
+#B cell areas: Blue colors:  B #1C3EE5, B CD21 high #1CA7E5
+#Marginal zones MZ: purple colors: Mono Ly6c low #D2E51C, mono ly6c high #E5A21C, Mac #EFCD8B, pDC #EFEF8B, Neut #F2EB08
 
 
 ########## Barplot of cell type proportions ##########
@@ -135,18 +143,18 @@ df_sphere_cell_type_pct$sphere <- rownames(df_sphere_cell_type_pct)
 ct1 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("B","sphere")]
 colnames(ct1) <- c("proportion","sphere")
 ct1$annotation <- "B"
-ct2 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("T_CD8","sphere")]
+ct2 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("CD8_T","sphere")]
 colnames(ct2) <- c("proportion","sphere")
-ct2$annotation <- "T_CD8"
+ct2$annotation <- "CD8_T"
 ct3 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("B_CD21_high","sphere")]
 colnames(ct3) <- c("proportion","sphere")
 ct3$annotation <- "B_CD21_high"
 ct4 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("Mono_Ly6c_low","sphere")]
 colnames(ct4) <- c("proportion","sphere")
 ct4$annotation <- "Mono_Ly6c_low"
-ct5 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("T_CD4","sphere")]
+ct5 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("CD4_T","sphere")]
 colnames(ct5) <- c("proportion","sphere")
-ct5$annotation <- "T_CD4"
+ct5$annotation <- "CD4_T"
 ct6 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("NK","sphere")]
 colnames(ct6) <- c("proportion","sphere")
 ct6$annotation <- "NK"
@@ -162,12 +170,17 @@ ct9$annotation <- "Neutrophils"
 ct10 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("pDC","sphere")]
 colnames(ct10) <- c("proportion","sphere")
 ct10$annotation <- "pDC"
-ct11 <- df_sphere_cell_type_pct[,colnames(df_sphere_cell_type_pct) %in% c("PC","sphere")]
-colnames(ct11) <- c("proportion","sphere")
-ct11$annotation <- "PC"
 
 #merge dataframes 
-ct <- do.call("rbind",list(paste0("ct",1:11)))
+ct <- rbind(ct1,ct2)
+ct <- rbind(ct,ct3)
+ct <- rbind(ct,ct4)
+ct <- rbind(ct,ct5)
+ct <- rbind(ct,ct6)
+ct <- rbind(ct,ct7)
+ct <- rbind(ct,ct8)
+ct <- rbind(ct,ct9)
+ct <- rbind(ct,ct10)
 
 #define order of spheres based on T cell zone percentage high to low 
 df_zone <- table(merged_cl$sphere,merged_cl$zone)
@@ -193,9 +206,12 @@ ct$sphere <- factor(ct$sphere, levels = rownames(df_zone_pct))
 
 ###Colours
 #T cell areas: red colors:  CD4 T #B50926, CD8 T#EA32EA , NK #F9AFC0
-#B cell areas: Blue colors:  B #1C3EE5, B CD21 high #1CA7E5, PC #1CE1E5
+#B cell areas: Blue colors:  B #1C3EE5, B CD21 high #1CA7E5
 #Marginal zones MZ: purple colors: Mono Ly6c low #D2E51C, mono ly6c high #E5A21C, Mac #EFCD8B, pDC #EFEF8B, Neut #F2EB08
 
+#reorder cell types for plotting 
+ct$annotation <- factor(ct$annotation,levels = c("B","B_CD21_high","CD4_T","CD8_T","NK", "Macrophages","Mono_Ly6c_high",
+                                                 "Mono_Ly6c_low","Neutrophils","pDC"))
 p <- ggplot(ct, aes(fill=annotation, y=proportion, x=sphere)) + theme_classic() +
   geom_bar(position="stack", stat="identity" ) + 
   ggtitle("Cell type per sphere")+  theme(axis.text = element_text(size = 5)) + 
@@ -203,8 +219,10 @@ p <- ggplot(ct, aes(fill=annotation, y=proportion, x=sphere)) + theme_classic() 
   theme(legend.title = element_text(size = 30), legend.text = element_text(size = 30))  + 
   xlab("Sphere") + ylab("Cell type proportion") + 
   theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(values=  c("#1C3EE5","#1CA7E5","#EFCD8B","#E5A21C","#D2E51C","#F2EB08","#F9AFC0","#1CE1E5",
-                               "#EFEF8B","#B50926","#EA32EA"))
+  scale_fill_manual(values=  c("#1C3EE5","#1CA7E5", "#B50926","#EA32EA", "#F9AFC0", "#EFCD8B","#E5A21C","#D2E51C","#F2EB08",
+                               "#EFEF8B"))
 p + ggsave("./figures/4/Proportion_per_sphere_legend.pdf", width = 15, height = 10)
 p + ggsave("./figures/4/Proportion_per_sphere_legend.svg", width = 15, height = 10)
+
+
 
