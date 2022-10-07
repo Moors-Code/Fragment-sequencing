226b7ba9e507988406c1b38bbe4f70cf2ecec69a
KristinaHandler <kristina.handler2806@gmail.com>
2022-10-07 11:11:11 +0200

    Update 2.Functions_Feature_area_integration.R

--- Highly_multiplexed_FISH_analysis/functions_and_packages/2.Functions_Feature_area_integration.R
+++ Highly_multiplexed_FISH_analysis/functions_and_packages/2.Functions_Feature_area_integration.R
@@ -7,3 +7,33 @@ extracting_x_y_info_Resolve <- function(
   csv_file$coordinate <- gsub("(.*),.*","\\1",csv_file$X.Y.Value)      
   output_vector <- csv_file$coordinate
 }
+
+
+###Function to generate text files with cell type annotation per slide for the overlay with the Dapi image in ImageJ 
+#it produces Text files for each slide or cell type with the Cell number ID in one column and the cell annotation in another column 
+text_file_generation_anno_resolve <- function(
+  seurat_object,
+  cell_type,
+  slide_name,
+  sample_name
+) {
+  Idents(seurat_object) <- "annotation"
+  text_file <- subset(seurat_object, idents = cell_type)
+  text_file <- as.data.frame(text_file@meta.data)
+  text_file <- text_file[,c("Cell","annotation")]
+  text_file$Cell <- gsub(slide_name, "Cell", rownames(text_file))
+  write.table(text_file, paste0(sample_name, "_", cell_type, ".txt"),row.names = FALSE, quote = F, sep = "\t")
+}
+
+text_file_generation_per_slide_resolve <- function(
+  seurat_object,
+  slide_name,
+  slide_name2
+) {
+  Idents(seurat_object) <- "Slide"
+  text_file <- subset(seurat_object, idents = slide_name)
+  text_file <- as.data.frame(text_file@meta.data)
+  text_file <- text_file[,c(10,24)]
+  text_file$Cell <- gsub(slide_name2, "Cell", rownames(text_file))
+  write.table(text_file, paste0(slide_name, "_", ".txt"),row.names = FALSE, quote = F, sep = "\t")
+}
